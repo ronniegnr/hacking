32f3d51433e30a52f72fcba0738f1f86c5591113
ronniegnr <gnr.ronnie@gmail.com>
2018-03-21 08:55:11 +0600

    Placed hackerrank solutions.

---
+++ src/bd/com/ronnie/hackerrank/datastructures/arrays/Sparse-Arrays/Solution.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        String[] arrString = new String[n];
+        for(int i=0; i<n; i++) {
+            arrString[i] = scanner.next();
+        }
+    
+        int q = scanner.nextInt();
+        String strQuery;
+        int[] result = new int[q];
+        int count=0;
+        for(int i=0; i<q; i++) {
+            strQuery = scanner.next();
+            for(int j=0; j<n; j++) {
+                if(strQuery.equals(arrString[j])) {
+                    count++;
+                }
+            }
+            result[i] = count;
+            count = 0;
+        }
+
+        for(int i=0; i<q; i++) {
+            System.out.println(result[i]);
+        }
+
+    }
+}
