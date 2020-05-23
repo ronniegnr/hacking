e8fe1428064fdc7f45e670592dbfe88c3f4d55a6
ronniegnr <gnr.ronnie@gmail.com>
2020-05-23 13:30:58 +0600

    first of codeforces

---
+++ src/bd/com/ronnie/hacking/codeforces/71A_Way_Too_Long_Words/Solution.java
@@ -0,0 +1,27 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        List<String> strs = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String input = in.next();
+            strs.add(input);
+        }
+
+        for (String s : strs) {
+            if(s.length() > 10) {
+                System.out.println("" + s.charAt(0) + (s.length()-2)  + s.charAt(s.length() - 1));
+            } else {
+                System.out.println(s);
+            }
+        }
+
+    }
+}
