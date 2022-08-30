9f98c24bd0413f47d41bf07211dda24eb9a880c3
ronniegnr <gnr.ronnie@gmail.com>
2022-08-30 16:02:31 +0600

    another git commit

--- src/bd/com/ronnie/hacking/Main.java
+++ src/bd/com/ronnie/hacking/Main.java
@@ -3,6 +3,6 @@ package bd.com.ronnie.hacking;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello world!!");
     }
 }
