dcf3227853209f192101ba0f8800833913323067
Ronnie <gnr.ronnie@gmail.com>
2023-03-04 10:06:37 +0600

    leetcode - valid-parentheses - not passing all the test cases

--- src/bd/com/ronnie/hacking/leetcode/valid_parentheses/Solution/Solution.java
+++ src/bd/com/ronnie/hacking/leetcode/valid_parentheses/Solution/Solution.java
@@ -1,9 +1,47 @@
 package bd.com.ronnie.hacking.leetcode.valid_parentheses.Solution;
 
+import java.util.Scanner;
+
 public class Solution {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println(isValid(s));
     }
+
+    public static boolean isValid(String s) {
+        int a = 0, b = 0, c = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            switch (s.charAt(i)) {
+                case '(':
+                    a++;
+                    break;
+                case ')':
+                    a--;
+                    break;
+                case '{':
+                    b++;
+                    break;
+                case '}':
+                    b--;
+                    break;
+                case '[':
+                    c++;
+                    break;
+                case ']':
+                    c--;
+                    break;
+            }
+        }
+
+        if (a == 0 && b == 0 && c == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
 
 // https://leetcode.com/problems/valid-parentheses/
