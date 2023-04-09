2aa7401fce9326f2cec2b9479739f58c38cacc1e
ronniegnr <gnr.ronnie@gmail.com>
2023-04-09 13:59:13 +0600

    leetcode - longest common prefix

--- src/bd/com/ronnie/hacking/leetcode/longest_common_prefix/Solution.java
+++ src/bd/com/ronnie/hacking/leetcode/longest_common_prefix/Solution.java
@@ -1,25 +1,50 @@
 package bd.com.ronnie.hacking.leetcode.longest_common_prefix;
 
-import java.util.Scanner;
-
 public class Solution {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        /*Scanner scanner = new Scanner(System.in);
         int testCases = scanner.nextInt();
         String[] strs = new String[testCases];
         for (int i = 0; i < testCases; i++) {
             strs[i] = scanner.nextLine();
-        }
+        }*/
+
+        String[] strs = new String[] {
+            "flower", "fow", "flo"
+        } ;
+
 
         System.out.println(longestCommonPrefix(strs));
     }
 
     public static String longestCommonPrefix(String[] strs) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < strs.length; i++) {
+        char c;
+        boolean isAdd;
 
+        for (int i = 0; ; i++) {
+            try {
+                c = strs[0].charAt(i);
+                isAdd = true;
+                for (int j = 1; j < strs.length; j++) {
+                    if (c != strs[j].charAt(i)) {
+                        isAdd = false;
+                        break;
+                    }
+                }
+                if (isAdd) {
+                    sb.append(c);
+                } else {
+                    break;
+                }
+            } catch (Exception exception) {
+                break;
+            }
         }
+
         return sb.toString();
+
+
     }
 }
 
